d3615090fe613efcb1d33165a0f0bd04bf56baa2
Kirti Bhosale <bhosalekirti2002@gmail.com>
2023-12-04 11:03:07 +0530

    update

--- run.py
+++ run.py
@@ -2,16 +2,3 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# from flask import Flask,jsonify
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/')
-# def welcome():
-#     return jsonify({'message': 'Welcome to the IntelliHireHub-Server'})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
